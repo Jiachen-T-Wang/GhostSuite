578cf681205677b468579f2c96e4dfda1cf564b6
Jiachen-T-Wang <tianhaowang@princeton.edu>
2026-01-03 10:56:31 -0500

    change use dummy bias to false

--- ghostEngines/engine_manager.py
+++ ghostEngines/engine_manager.py
@@ -73,7 +73,7 @@ class GhostEngineManager:
             module=self.model,
             val_batch_size=self.config.val_batch_size,
             loss_reduction='mean',
-            use_dummy_bias=True,
+            use_dummy_bias=False,
             dot_prod_save_path=dot_prod_save_path
         )
         
